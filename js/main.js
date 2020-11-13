52018211fb643a95480d6ef69c2768e7b3e9b88f
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2020-11-13 18:17:10 +0800

    nav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-// menu show and hidden
 const navMenu = document.getElementById('nav-menu')
 const toggleMenu = document.getElementById('nav-toggle')
 const closeMenu = document.getElementById('nav-close')
 
-// show
+/*SHOW*/ 
 toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show')
     navMenu.classList.remove('hidden')
 })
 
-// hidden
+/*HIDDEN*/
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('hidden')
     navMenu.classList.remove('show')
